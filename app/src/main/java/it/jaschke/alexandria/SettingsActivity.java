7e393f6e4adc7f70ed5b89c9203a26ab6c0332ae
Laurent Direr <laurent.direr@gmail.com>
2015-11-26 17:02:13 +0700

    Adding display of summary value to the settings activity.

--- app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
+++ app/src/main/java/it/jaschke/alexandria/SettingsActivity.java
@@ -1,18 +1,41 @@
 package it.jaschke.alexandria;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 /**
  * Created by saj on 27/01/15.
  */
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    private static final String LOG_TAG = SettingsActivity.class.getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+        ListPreference startScreenPreference = (ListPreference) findPreference(getString(R.string.pref_start_fragment_key));
+        startScreenPreference.setOnPreferenceChangeListener(this);
+        startScreenPreference.setSummary(startScreenPreference.getEntry());
+    }
 
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String newValueStr = newValue.toString();
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int newValueIndex = listPreference.findIndexOfValue(newValueStr);
+            // The state of the preference is updated AFTER onPreferenceChange, so we can't use the convenient getEntry()
+            listPreference.setSummary(listPreference.getEntries()[newValueIndex]);
+        }
+        else {
+            preference.setSummary(newValue.toString());
+        }
+        return true;
     }
 }
